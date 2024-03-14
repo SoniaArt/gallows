f4ec42e0c42df07253db171d3d44ac89f642a654
Sonia <sofaartyushina@gmail.com>
2024-03-14 12:30:18 +0300

    Теперь можно повторно начать игру

--- praktika1/praktika1.cpp
+++ praktika1/praktika1.cpp
@@ -10,15 +10,11 @@
 //сделать прокрутку, бонусы, возможность обратиться к правилам в любой момент
 //найти точно правильные правила
 //использовать псевдографику
+//разобраться с регистрами (чтобы оба учитывались)
 void Rules() {
 	printf("  Правила игры: \n Игорк должен отдагать словно по буквам на выбранную тему.\n На это ему даётся 6 попыток. Если игрок ошибся 6 раз, то он проигрывает.\n");
 }
-int main() {
-	setlocale(LC_ALL, "Rus");
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
-	srand(time(NULL));
-	printf("                                                   Виселица\n");
+void play() {
 	Rules();
 	char towns[5][MaxM] = { "Москва", "Владивосток", "Казань", "Омск", "Смоленск" };
 	char animals[5][MaxM] = { "Собака", "Мышь", "Кошка", "Курица", "Лошадь" };
@@ -26,27 +22,27 @@ int main() {
 	char countries[5][MaxM] = { "Аргентина", "Гаити", "Мадагаскар", "Турция", "Румыния" };
 	char word[20]; //слово, с которым работаем
 	char word_guess[20]; //маска для угадывания
-	int theme, tries=0, MaxTries=6, num_word, word_len, found = 0;
+	int theme, tries = 0, MaxTries = 6, num_word, word_len, found = 0;
 	do {
 		printf("Выберите тему: \n1.Города \n2.Животные \n3.Растения \n4.Страны\n");
 		printf("Ваш выбор: "); scanf_s("%d", &theme);
 		if ((theme < 1) || (theme > 5)) { printf("Некоректно выбрана котегория. Повторите попытку.\n"); }
 	} while ((theme < 1) || (theme > 5));
 	switch (theme) {
-	case 1: 
-		num_word = rand() % 4; 
+	case 1:
+		num_word = rand() % 4;
 		strcpy_s(word, towns[num_word]);
 		break;
-	case 2: 
+	case 2:
 		num_word = rand() % 4;
 		strcpy_s(word, animals[num_word]);
 		break;
-	case 3: 
-		num_word = rand() % 4; 
+	case 3:
+		num_word = rand() % 4;
 		strcpy_s(word, plants[num_word]);
 		break;
-	case 4: 
-		num_word = rand() % 4; 
+	case 4:
+		num_word = rand() % 4;
 		strcpy_s(word, countries[num_word]);
 		break;
 	}
@@ -76,11 +72,27 @@ int main() {
 			found = 1;
 		}
 	}
-		if (found == 1) {
-			printf("Вы отгадали слово! %s\n", word);
+	if (found == 1) {
+		printf("Вы отгадали слово! %s\n", word);
+	}
+	else {
+		printf("Вы проиграли. Загаданное слово было: %s\n", word);
+	}
+}
+int main() {
+	setlocale(LC_ALL, "Rus");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	srand(time(NULL));
+	printf("                                                   Виселица\n");
+	int t;
+	do {
+		printf("Если вы хотите начать игру, введите 1. Если вы хотите выйти, введите 0: "); scanf_s("%d", &t);
+		switch (t) {
+		case 1: play(); break;
+		case 0: printf("Выход"); break;
+		default: printf("Некорректный ввод. Повторите попытку.\n"); break;
 		}
-		else {
-			printf("Вы проиграли. Загаданное слово было: %s\n", word);
-		}
+	} while (t != 0);
 	return 0;
 }
